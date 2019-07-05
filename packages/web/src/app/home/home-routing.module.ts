568be62f265ff49b91254357b276c346382fe4d6
James Salas <osdev@jamesbs.com>
2019-07-04 22:09:40 -0400

    fix login test

--- packages/web/src/app/home/home-routing.module.ts
+++ packages/web/src/app/home/home-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { isAuthenticatedMatcher } from './is-authenticated.matcher';
 
-const routes: Routes = [
+export const routes: Routes = [
   {
     matcher: isAuthenticatedMatcher,
     loadChildren: () => import('../dash/dash.module')
