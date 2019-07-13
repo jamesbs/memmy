7da5f4ca540d75379c96e1d7e11e93c2d40ab69e
James Salas <osdev@jamesbs.com>
2019-07-13 14:13:30 -0400

    remove stored credentials when logging out

--- packages/web/src/app/model/state/store/server/server-credentials.ts
+++ packages/web/src/app/model/state/store/server/server-credentials.ts
@@ -1,10 +1,12 @@
 import { createReducer, on } from '@ngrx/store';
 import { loginSuccessful } from '../../action/login';
 import { ServerCredentials, asServerCredentials } from '@memmy/model';
+import { logoutSuccessful } from '../../action/logout';
 
 export const initialServerCredentialsState: ServerCredentials | undefined = undefined;
 
 export const serverCredentialsReducer = createReducer<ServerCredentials | undefined>(
   initialServerCredentialsState,
   on(loginSuccessful, (_, action) => asServerCredentials(action)),
+  on(logoutSuccessful, () => initialServerCredentialsState),
 );
