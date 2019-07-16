daced692d0d3f558b4b74000f29ba3f404d69636
James Salas <osdev@jamesbs.com>
2019-07-16 13:49:50 -0400

    use fixtures for server state

--- packages/web/cypress/integration/dashboard.spec.js
+++ packages/web/cypress/integration/dashboard.spec.js
@@ -1,17 +1,25 @@
 describe('dashboard', () => {
-  it('clicking a link goes to the gallery', () => {
-    cy.window().then(window => {
-      window.localStorage.setItem('server', JSON.stringify({ credentials: { token: "Y7s6tGAHtkjkFmTV" } }));
+  describe('page', () => {
+    it('clicking a link goes to the gallery', () => {
+      cy.fixture('is-authenticated.json').then(serverState => {
+        cy.window().then(window => {
+          window.localStorage.setItem('server', JSON.stringify(serverState));
+        });
+      });
+     
+      cy.visit(Cypress.env('appEndpoint'));
+      cy.contains('Christmas 2016').click();
+      cy.url().should('contain', 'gallery');
     });
-    cy.visit(Cypress.env('appEndpoint'));
-    cy.contains('Christmas 2016').click();
-    cy.url().should('contain', 'gallery');
-  });
+  })
 
   describe('action bar', () => {
     it('logs out when you click the logout button', () => {
-      cy.window().then(window => {
-        window.localStorage.setItem('server', JSON.stringify({ credentials: { token: "Y7s6tGAHtkjkFmTV" } }));
+
+      cy.fixture('is-authenticated.json').then(serverState => {
+        cy.window().then(window => {
+          window.localStorage.setItem('server', JSON.stringify(serverState));
+        });
       });
       cy.visit(Cypress.env('appEndpoint'));
       cy.get('[title=logout]').click();
